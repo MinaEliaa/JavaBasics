da626f6c298037292395d21c13215bdca1f7b38d
Mina Elia Louize <107637818+MinaEliaa@users.noreply.github.com>
2022-11-02 23:32:09 -0700

    remove comments

--- weightofWatermelon.java
+++ weightofWatermelon.java
@@ -1,15 +1,7 @@
 
 import java.util.Scanner;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Mena
- */
 public class weightofWatermelon {
     
     public static void main(String[] args) {
